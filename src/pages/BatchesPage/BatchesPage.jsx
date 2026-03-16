97cbd44ddf5483b793bf7c16d6e1de17dfaa8f85
Ruslan_Sokolov <vaquero1master@gmail.com>
2026-03-16 12:41:25 +0100

    correct modal windows

--- src/pages/BatchesPage/BatchesPage.jsx
+++ src/pages/BatchesPage/BatchesPage.jsx
@@ -295,6 +295,7 @@ const BatchesPage = () => {
                                     type="button"
                                     className={styles.modalBtnCreate}
                                     onClick={handleCreateBatch}
+                                    disabled={!batchName}
                                 >
                                     Create
                                 </button>
