181e11693c601237c5207dede7c894f63c353791
Ruslan_Sokolov <vaquero1master@gmail.com>
2026-03-16 20:27:34 +0100

    pages animation

---
+++ src/components/PageTransition/PageTransition.jsx
@@ -0,0 +1,7 @@
+import styles from "./PageTransition.module.css";
+
+const PageTransition = ({ children }) => {
+  return <div className={styles.wrapper}>{children}</div>;
+};
+
+export default PageTransition;
